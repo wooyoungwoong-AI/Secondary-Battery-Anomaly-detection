10934061ec5144789d620d751c4f0a16f3acfc1d
youngwoong woo <uyeongung06@gmail.com>
2024-06-21 00:15:11 +0900

    Resolving the formula error

--- notebook/py/Training.py
+++ notebook/py/Training.py
@@ -39,7 +39,7 @@ def prepare_data_loaders(dataset, batch_size, shuffle=True):
 
 def loss_function(x, x_hat, mu, sigma):
     x, x_hat, mu, sigma = x.cpu(), x_hat.cpu(), mu.cpu(), sigma.cpu()
-    restore_error = -torch.sum(x * torch.log(x_hat + 1e-12) + (1 - x) * torch.log(1 - x_hat + 1e-12), dim=(1, 2, 3))
+    restore_error = torch.nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
     kl_divergence = 0.5 * torch.sum(mu**2 + sigma**2 - torch.log(sigma**2 + 1e-12) - 1, dim=1)
     return torch.mean(restore_error + kl_divergence), torch.mean(restore_error), torch.mean(kl_divergence)
 
