f183090c574b5d4025d51dd1f484fca943472609
youngwoong woo <uyeongung06@gmail.com>
2024-06-24 17:26:24 +0900

    no using linaer

--- notebook/py/vae.py
+++ notebook/py/vae.py
@@ -3,11 +3,10 @@ import torch.nn as nn
 import torch.optim as optim
 
 class NeuralNet:
-    def __init__(self, height, width, channel, device, ngpu, ksize, z_dim, avg_pool,learning_rate=1e-3):
+    def __init__(self, height, width, channel, device, ngpu, ksize, z_dim, learning_rate=1e-3):
         self.height, self.width, self.channel = height, width, channel
         self.device, self.ngpu = device, ngpu
         self.ksize, self.z_dim, self.learning_rate = ksize, z_dim, learning_rate
-        self.avg_pool = avg_pool
 
         self.encoder = (
             Encoder(height=self.height, width=self.width, channel=self.channel,
@@ -16,7 +15,7 @@ class NeuralNet:
 
         self.decoder = (
             Decoder(height=self.height, width=self.width, channel=self.channel,
-                    ngpu=self.ngpu, ksize=self.ksize, z_dim=self.z_dim, avg_pool=self.avg_pool).to(self.device)
+                    ngpu=self.ngpu, ksize=self.ksize, z_dim=self.z_dim).to(self.device)
         )
 
         self.models = [self.encoder, self.decoder]
@@ -31,7 +30,7 @@ class NeuralNet:
         for idx_m, model in enumerate(self.models):
             for p in model.parameters():
                 self.num_params += p.numel()
-        print(f"The number of parameters: {self.num_params}")
+        print(f"The number of parameters: {self.num_params:.4f}")
 
         self.params = list(self.encoder.parameters()) + list(self.decoder.parameters())
         # self.optimizer = optim.Adam(self.params, lr=self.learning_rate)
@@ -82,48 +81,54 @@ class Encoder(nn.Module):
             nn.ELU(),
         )
 
-        self.encoder_dense = nn.Sequential(
-            Flatten(),
-            nn.Linear(123 * 123 * 64, 512),
-            nn.ELU(),
-            nn.Linear(512, self.z_dim*2)
-        )
+        self.conv_mu = nn.Conv2d(64, z_dim, kernel_size=1)
+        self.conv_sigma = nn.Conv2d(64, z_dim, kernel_size=1)
 
-    def split_z(self, z):
-        z_mu = z[:, :self.z_dim]
-        z_sigma = z[:, self.z_dim:]
+        # self.encoder_dense = nn.Sequential(
+        #     Flatten(),
+        #     nn.Linear(123 * 123 * 64, 512),
+        #     nn.ELU(),
+        #     nn.Linear(512, self.z_dim*2)
+        # )
 
-        return z_mu, z_sigma
+    # def split_z(self, z):
+    #     z_mu = z[:, :self.z_dim]
+    #     z_sigma = z[:, self.z_dim:]
+
+    #     return z_mu, z_sigma
     
-    #reparameterzation
-    def sample_z(self, mu, sigma):
-        epsilon = torch.randn_like(mu)
-        sample = mu + (sigma * epsilon)
+    # #reparameterzation
+    # def sample_z(self, mu, sigma):
+    #     epsilon = torch.randn_like(mu)
+    #     sample = mu + (sigma * epsilon)
 
-        return sample
+    #     return sample
     
     def forward(self, input):
         conv_out = self.encoder_conv(input)
-        # print(conv_out.shape) 32, 64, 123, 123
-        z_params = self.encoder_dense(conv_out)
-        z_mu, z_sigma = self.split_z(z_params)
-        z_sample = self.sample_z(mu=z_mu, sigma=z_sigma)
-        # print(z_sample.shape) 32, 20
+        mu = self.conv_mu(conv_out)
+        sigma = torch.exp(0.5 * self.conv_sigma(conv_out))
+        epsilon = torch.randn_like(sigma)
+        z_sample = mu + sigma * epsilon
 
-        return z_sample, z_mu, z_sigma
+        return z_sample, mu, sigma
     
 class Decoder(nn.Module):
-    def __init__(self, height, width, channel, ngpu, ksize, z_dim, avg_pool):
+    def __init__(self, height, width, channel, ngpu, ksize, z_dim):
         super(Decoder, self).__init__()
 
         self.height, self.width, self.channel = height, width, channel
         self.ngpu, self.ksize, self.z_dim = ngpu, ksize, z_dim
-        self.avg_pool = avg_pool
+
+        # self.decoder_dense = nn.Sequential(
+        #     nn.Linear(self.z_dim, 512),
+        #     nn.ELU(),
+        #     nn.Linear(512, (self.height//(2**2))*(self.width//(2**2))*64),
+        #     nn.ELU()
+        # )
 
         self.decoder_dense = nn.Sequential(
-            nn.Linear(self.z_dim, 512),
-            nn.ELU(),
-            nn.Linear(512, (self.height//(2**2))*(self.width//(2**2))*64),
+            nn.Conv2d(in_channels=z_dim, out_channels=64, kernel_size=1),
             nn.ELU()
         )
 
@@ -132,24 +137,20 @@ class Decoder(nn.Module):
             nn.ELU(),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=self.ksize, stride=1, padding=self.ksize//2),
             nn.ELU(),
-
             nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=self.ksize+1, stride=2, padding=1, output_padding=1),
             nn.ELU(),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=self.ksize, stride=1, padding=self.ksize//2),
             nn.ELU(),
-
             nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=self.ksize+1, stride=2, padding=1, output_padding=1),
             nn.ELU(),
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=self.ksize, stride=1, padding=self.ksize//2),
             nn.ELU(),
-            nn.Conv2d(in_channels=16, out_channels=self.channel, kernel_size=self.ksize, stride=1, padding=ksize//2),
-            nn.Sigmoid(),
+            nn.Conv2d(in_channels=16, out_channels=self.channel, kernel_size=self.ksize, stride=1, padding=self.ksize//2),
+            # nn.AdaptiveAvgPool2d((height, width)),
+            nn.Sigmoid()
         )
-
+    
     def forward(self, input):
         dense_out = self.decoder_dense(input)
-        dense_res = dense_out.view(dense_out.size(0), 64, (self.height//(2**2)), (self.width//(2**2)))
-        x_hat = self.decoder_conv(dense_res)
-        # print(x_hat.shape) torch.Size([32, 1, 498, 498])
-        x_hat = self.avg_pool(x_hat)
+        x_hat = self.decoder_conv(dense_out)
         return x_hat
